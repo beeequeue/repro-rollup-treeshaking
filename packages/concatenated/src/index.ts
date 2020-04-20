b0c84fc78f1acc776ca2d26b6afaa13b2ddda1ee
Adam <adam.lj.haglund@gmail.com>
2020-04-20 14:05:35 +0200

    add non-preserved

---
+++ packages/concatenated/src/index.ts
@@ -0,0 +1,2 @@
+export * from "./data/one";
+export * from "./data/two";
--- packages/preserved-modules/tsdx.config.js
+++ packages/preserved-modules/tsdx.config.js
@@ -1,13 +1,5 @@
 module.exports = {
   rollup(config, opts) {
-    const originalExternalCheck = config.external
-
-    config.external = (id) => {
-      if (id === "@beequeueue/data") return false
-
-      return originalExternalCheck(id)
-    }
-
     if (opts.format === 'esm') {
       config = { ...config, preserveModules: true }
       config.output = { ...config.output, dir: 'dist/', entryFileNames: '[name].esm.js' }
